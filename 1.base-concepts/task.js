3d7dd1624b9be141374195f1f5258e4ec87bcfa4
Lopyhof <xaxaevgesha@mail.ru>
2022-08-25 22:10:38 +0700

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,85 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
+    let arr = [];
+    let a = 1;
+    let b = 2;
+    let c = 1;
+    let d;
+    let d1;
+    let d2;
+    
+    d = b ** 2 - 4 * a * c;
+    console.log('Дискриминант равен ', d);
+    
+    if (d < 0) {
+        arr = []
+        console.log('Корней нет: ', arr);
+    } else if (d === 0) {
+        d = -b / (2 * a);
+        arr.push(d);
+        console.log('Корень уравнения: ', arr);
+    } else {
+        d1 = (-b + Math.sqrt(d) )/(2*a);
+        arr.push(d1);
+        d2 = (-b - Math.sqrt(d) )/(2*a);
+        arr.push(d2);
+        console.log('Корни уравнения: ', arr);
+    }
+        
   // код для задачи №1 писать здесь
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+let credit = {
+  persent: '16',
+  summBegin: 12,
+  summCredit: 6000,
+  time: 24
+};
 
-  // код для задачи №2 писать здесь
+if (credit.persent != Number(credit.persent)) {
+  console.log ('Параметр "процентная ставка" содержит неправильное значение "',credit.persent,'"');
+} else {
+  credit.persent = Number(credit.persent);
+  console.log ('Ваша процентная ставка: ', credit.persent);
+};
 
-  return totalAmount;
+if (credit.summBegin != Number(credit.summBegin)) {
+  console.log ('Параметр "сумма первоначального взноса" содержит неправильное значение "',credit.summBegin,'"');
+} else {
+  credit.summBegin = Number(credit.summBegin);
+  console.log ('Ваш первоначальный взнос: ', credit.summBegin);
+};
+
+if (credit.summCredit != Number(credit.summCredit)) {
+  console.log ('Параметр "сумма кредита" содержит неправильное значение "',credit.summCredit,'"');
+} else {
+  credit.summCredit = Number(credit.summCredit);
+  console.log ('Сумма кредита: ', credit.summCredit);
+};
+
+if (credit.time != Number(credit.time)) {
+  console.log ('Параметр "срок погашения кредита" содержит неправильное значение "',credit.time,'"');
+} else {
+  credit.time = Number(credit.time);
+  console.log ('Срок погашения кредита: ', credit.time, 'месяца');
+};
+
+function allCredit(credit) {
+  let creditBody;
+  let monthPay;
+  let P;
+  let allSumm;
+  creditBody = credit.summCredit - credit.summBegin;
+  console.log('Банку необходимо будет вернуть сумму в размере: ', creditBody);
+  P = credit.persent * (1 / 12);
+  monthPay = creditBody * (P + (P / ((1 + P) ** credit.time) - 1));
+  console.log('Ежемесячный платеж по кредиту составит: ', monthPay.toFixed(2));
+  allSumm = monthPay * credit.time;
+  console.log('Общая сумма, которую Вам придется заплатить, составит: ', allSumm.toFixed(2));
+};
+
+allCredit(credit);
+
 }
